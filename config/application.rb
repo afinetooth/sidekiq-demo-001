5787cccfc5d8dd98f108c98f5b88680845ec0011
James Kessler <afinetooth@gmail.com>
2018-02-24 20:19:56 -0800

    Added sidekiq gem. Added new sample job, HardJob, and new sample worker, HardWorker.

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,8 @@ module DemoSidekiq001
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    
+    # Use sidekiq as ActiveJob job runner
+    config.active_job.queue_adapter = :sidekiq
   end
 end
