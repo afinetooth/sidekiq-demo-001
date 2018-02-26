d9f83f7ec2a22d3956b3439dad05b3d90cf80fce
James Kessler <afinetooth@gmail.com>
2018-02-26 14:53:04 -0800

    Modify expression for log message in HardJob. Not necessary to use Rail.logger due to context and removing “Rails” makes logging look more parallel between Jobs and Workers and helps in explaining logging differences between them. (See README.)

--- app/jobs/hard_job.rb
+++ app/jobs/hard_job.rb
@@ -2,8 +2,7 @@ class HardJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
-    # Example from: http://bica.co/2015/03/08/using-active-job-with-sidekiq/
-    Rails.logger.debug "#{self.class.name}: I'm performing my job with arguments: #{args.inspect}"
+    # Do something
+    logger.debug "#{self.class.name}: I'm performing my job with arguments: #{args.inspect}"
   end
 end
